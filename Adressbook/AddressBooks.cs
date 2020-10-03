b17a469c6c964f132daaaee4de651197588d6a3e
Priya Biriya <priyabiriya1707@gmail.com>
2020-10-03 23:57:50 +0530

    [Priya] add . Multiple Address Book

---
+++ Adressbook/AddressBooks.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+namespace AddressBook
+{
+    class AddressBooks
+    {
+        string _name;
+        Dictionary<string, AddressBookMain> _multContactDetails = new Dictionary<string, AddressBookMain>();
+        public AddressBooks()
+        {
+            this._name = "General";
+        }
+        public AddressBooks(string name)
+        {
+            this._name = name;
+        }
+        public string Name { get => _name; set => _name = value; }
+        public void AddNewAddressBook()
+        {
+            AddressBookMain addressBook = new AddressBookMain();
+            int numDetails;
+            Console.Write("Enter number of Contact Details you want to save : ");
+            numDetails = Int32.Parse(Console.ReadLine());
+            for (int i = 1; i <= numDetails; i++)
+                addressBook.AddContactDetails();
+            _multContactDetails.Add(this._name, addressBook);
+        }
+        public void EditDetailsInAddressBook()
+        {
+            _multContactDetails[Name].EditContactDetails();
+        }
+        public void DeleteOneContactDetail()
+        {
+            _multContactDetails[Name].DeleteContactDetails();
+        }
+        public void DisplayContactsInCurrentAddressBook()
+        {
+            _multContactDetails[Name].DisplayAllContacts();
+        }
+    }
+}
--- Adressbook/Program.cs
+++ Adressbook/Program.cs
@@ -1,12 +1,78 @@
 ﻿using System;
-
-namespace Adressbook
+using System.Collections.Generic;
+using System.Text;
+namespace AddressBook
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome To Address Book");
+            Console.WriteLine("Welcome to Address Book");
+            Console.WriteLine("========================");
+            AddressBooks addressBooksCollection = new AddressBooks();
+            addressBooksCollection.Name = "General";
+            bool contAddressBook = true; ;
+            bool contContactPanel = true; ;
+            do
+            {
+                Console.WriteLine("Enter\n" +
+                "1 : To Add a new Address Book\n" +
+                "2 : To use current address books ( " + addressBooksCollection.Name + " )\n" +
+                "3 : Switch Address Book\n" +
+                "0 : Exit");
+                int userChoice = Int32.Parse(Console.ReadLine());
+                switch (userChoice)
+                {
+                    case 1:
+                        contContactPanel = true;
+                        Console.WriteLine("Add Name of the new Address Book");
+                        addressBooksCollection.Name = Console.ReadLine();
+                        break;
+                    case 2:
+                        contContactPanel = true;
+                        break;
+                    case 3:
+                        contContactPanel = true;
+                        Console.WriteLine("Enter Name of the Address Book you want to switch");
+                        addressBooksCollection.Name = Console.ReadLine();
+                        break;
+                    case 0:
+                        contAddressBook = false;
+                        contContactPanel = false;
+                        break;
+                    default:
+                        Console.WriteLine("Wrong Option Entered");
+                        break;
+                }
+                int choice;
+                while (contContactPanel)
+                {
+                    Console.WriteLine("Enter\n" +
+                    "1 : Add Contact Details to " + addressBooksCollection.Name + " Address Book\n" +
+                    "2 : Edit a Contact Detail\n" +
+                    "3 : Delete a Contact Detail\n" +
+                    "4 : Exit");
+                    choice = Int32.Parse(Console.ReadLine());
+                    switch (choice)
+                    {
+                        case 1:
+                            addressBooksCollection.AddNewAddressBook();
+                            break;
+                        case 2:
+                            addressBooksCollection.EditDetailsInAddressBook();
+                            break;
+                        case 3:
+                            addressBooksCollection.DeleteOneContactDetail();
+                            break;
+                        case 4:
+                            contContactPanel = false;
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            } while (contAddressBook);
+            return;
         }
     }
 }
