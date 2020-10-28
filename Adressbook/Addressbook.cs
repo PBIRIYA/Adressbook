3b7b613621b7e4071d8b4739e55dd3d4d454693d
Priya Biriya <priyabiriya1707@gmail.com>
2020-10-29 02:05:01 +0530

    [Priya] add . get number of contact

--- Adressbook/Addressbook.cs
+++ Adressbook/Addressbook.cs
@@ -1,104 +1,53 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-using System.Linq;
-using System.Collections;
-namespace Addressbook
+
+namespace AddressBookSystem
 {
-    public class AddressBook
+    class AddressBook
     {
-        public HashSet<Contact> ContactSet = new HashSet<Contact>();
-        public ArrayList ContactList = new ArrayList();
-        List<Contact> Person = new List<Contact>();
-        HashSet<string> ContactName = new HashSet<string>();
-        public string FirstName { get; private set; }
-        public string LastName { get; private set; }
-        public void AddPerson()
+        public HashSet<Contact> People;
+        public AddressBook()
         {
-            Contact contact = new Contact();
-            Console.Write("Enter First Name: ");
-            contact.FirstName = Console.ReadLine();
-            Console.Write("Enter Last Name: ");
-            contact.LastName = Console.ReadLine();
-            Console.Write("Enter Address : ");
-            contact.Address = Console.ReadLine();
-            Console.Write("Enter City : ");
-            contact.City = Console.ReadLine();
-            Console.Write("Enter State : ");
-            contact.State = Console.ReadLine();
-            Console.Write("Enter Zip Code : ");
-            contact.ZipCode = Console.ReadLine();
-            Console.Write("Enter Phone Number : ");
-            contact.Phone = Console.ReadLine();
-            Console.Write("Enter Email ID : ");
-            contact.Email = Console.ReadLine();
-            Person.Add(contact);
-            ContactSet.Add(contact);
-            ContactName.Add(contact.FirstName);
-            ContactName.Add(contact.LastName);
+            People = new HashSet<Contact>();
         }
-        public bool CheckDuplicate()
+        public Contact FindContact(string fname)
         {
-            if (ContactName.Contains(FirstName) && ContactName.Contains(LastName))
+            Contact contact = null;
+            foreach (var person in People)
             {
-                Console.WriteLine("Contact details for this person already stored.");
+                if (person.FirstName.Equals(fname))
+                {
+                    contact = person;
+                    break;
+                }
+            }
+            return contact;
+        }
+        public bool AddContact(string FirstName, string LastName, string Address, string City, string State, string ZipCode, string PhoneNumber, string Email)
+        {
+            Contact contact = new Contact(FirstName, LastName, Address, City, State, ZipCode, PhoneNumber, Email);
+            Contact result = FindContact(FirstName);
+            if (result == null)
+            {
+                People.Add(contact);
+                return true;
+            }
+            else
+                return false;
+        }
+        public bool RemoveContact(string name)
+        {
+            Contact c = FindContact(name);
+
+            if (c != null)
+            {
+                People.Remove(c);
                 return true;
             }
             else
             {
-                ContactName.Add(FirstName);
-                ContactName.Add(LastName);
                 return false;
-            }
-        }
-        public Contact FindPerson(string firstName)
-        {
-            Contact toFind = Person.Find((person) => person.FirstName == firstName);
-            return toFind;
-
-        }
-        public void EditContactDetails()
-        {
-            string firstName = Console.ReadLine();
-            Contact editContact = FindPerson(firstName);
-            if (editContact == null)
-            {
-                Console.WriteLine("Not Found :" + firstName);
-            }
-            else
-            {
-                Contact EditContactDetails = new Contact();
-                Console.Write("Enter First Name: ");
-                EditContactDetails.FirstName = Console.ReadLine();
-                Console.Write("Enter Last Name: ");
-                EditContactDetails.LastName = Console.ReadLine();
-                Console.Write("Enter Address : ");
-                EditContactDetails.Address = Console.ReadLine();
-                Console.Write("Enter City : ");
-                EditContactDetails.City = Console.ReadLine();
-                Console.Write("Enter State : ");
-                EditContactDetails.State = Console.ReadLine();
-                Console.Write("Enter Zip Code : ");
-                EditContactDetails.ZipCode = Console.ReadLine();
-                Console.Write("Enter Phone Number : ");
-                EditContactDetails.Phone = Console.ReadLine();
-                Console.Write("Enter Email ID : ");
-                EditContactDetails.Email = Console.ReadLine();
-            }
-        }
-        public void DeleteContactDetails()
-        {
-            string firstName = Console.ReadLine();
-            Contact deleteContact = FindPerson(firstName);
-            if (deleteContact == null)
-            {
-                Console.WriteLine("Not Found :" + firstName);
-            }
-            else
-            {
-                Person.Remove(deleteContact);
-                Console.WriteLine("delete Contact successfully", firstName);
-                ContactList.Remove(Person);
             }
         }
     }
