5b0cf4e61028c188c980111f384744ef7525d090
Priya Biriya <priyabiriya1707@gmail.com>
2020-10-28 23:34:23 +0530

    [Priya] add . adding files

--- Adressbook/SearchList.cs
+++ Adressbook/SearchList.cs
@@ -1,10 +1,26 @@
-﻿using System;
+﻿using Addressbook;
+using System;
 using System.Collections.Generic;
 using System.Text;
-
 namespace Adressbook
 {
     class SearchList
     {
+        public Dictionary<string, HashSet<Contact>> GeneralDictionary = new Dictionary<string, HashSet<Contact>>();
+        public static List<Contact> SearchByCity()
+        {
+            Contact contact = new Contact();
+            List<Contact> ContactsByCity = new List<Contact>();
+            Dictionary<string, List<Contact>> CityDictionary = new Dictionary<string, List<Contact>>();
+            Console.WriteLine("Enter the city: ");
+            string city = Console.ReadLine();
+            if (contact.City == city)
+            {
+                ContactsByCity.Add(contact);
+                CityDictionary.Add(city, ContactsByCity);
+            }
+            Console.WriteLine(ContactsByCity);
+            return ContactsByCity;
+        }
     }
 }
